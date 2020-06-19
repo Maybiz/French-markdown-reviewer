843fda6b4c48466398a6b7d23f2f36ad66015de9
Maybiz <maxime.julliant@laposte.net>
2020-06-19 15:16:55 +0200

    add html mode

--- src/component/Preview.js
+++ src/component/Preview.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { connect } from 'react-redux'
 import marked from 'marked'
 
@@ -9,9 +9,28 @@ const mapStateToProps = state => {
 }
 
 const Preview = props => {
+
+	const [htmlMode, setHtmlMode] = useState(false)
+
+	const handleMode = () => setHtmlMode(!htmlMode)
+
+	useEffect(() => {
+
+		const preview = document.getElementById('preview')
+
+		!htmlMode ? (preview.innerHTML = marked(props.input.reducers)) : (preview.textContent = marked(props.input.reducers))
+
+	}, [htmlMode])
+
+	
+
 	return (
 		<Fragment>
-			<h2 className="preview__header">Preview</h2>
+			<div className="preview__header">
+				<h2>Preview</h2>
+				<button className="htmlMode" onClick={handleMode}>Mode HTML</button>
+			</div>
+			
 			<div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.input.reducers) }}></div>
 		</Fragment>
 	)
