325b6cf7564f2be2808d61095a3a06326d3a605a
Maybiz <maxime.julliant@laposte.net>
2020-06-22 07:32:27 +0200

    adjustments

--- src/component/Preview.js
+++ src/component/Preview.js
@@ -8,32 +8,38 @@ const mapStateToProps = state => {
 	}
 }
 
-const Preview = props => {
+marked.setOptions({ breaks: true })
 
+const Preview = props => {
 	const [htmlMode, setHtmlMode] = useState(false)
 
 	const handleMode = () => setHtmlMode(!htmlMode)
 
 	useEffect(() => {
-
 		const preview = document.getElementById('preview')
 
 		!htmlMode ? (preview.innerHTML = marked(props.input.reducers)) : (preview.textContent = marked(props.input.reducers))
+	}, [htmlMode, props.input.reducers])
 
-	}, [htmlMode])
-
-	
+	marked.setOptions({
+		breaks: true,
+		headerIds: false
+	})
 
 	return (
 		<Fragment>
 			<div className="preview__header">
 				<h2>Preview</h2>
-				<button className="htmlMode" onClick={handleMode}>Mode HTML</button>
+				<button className="htmlMode" onClick={handleMode}>
+					Mode HTML
+				</button>
 			</div>
-			
-			<div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.input.reducers) }}></div>
+
+			<div id="preview"></div>
 		</Fragment>
 	)
 }
 
 export default connect(mapStateToProps, null)(Preview)
+
+//{ __html: marked(props.input.reducers) }
